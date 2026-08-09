ec579bd036c3c408426c9f59ab0615987528374a
Laukik Dudhe <laukikdudhe31@gmail.com>
2026-08-09 18:43:58 +0530

    Add files via upload

---
+++ Question 25 - Inverted Right Triangle.py
@@ -0,0 +1,21 @@
+# Problem:
+# Print the following pattern:
+# *****
+# ****
+# ***
+# **
+# *
+# Rules:
+# 1. Use only while loops.
+# 2. Do NOT use a for loop.
+# 3. Use two while loops (Nested Loop).
+#Solution:-
+
+i=5
+while(i>=1):
+    j=1
+    while(j<=i):
+        print("*", end="")
+        j=j+1
+    print()
+    i=i-1
