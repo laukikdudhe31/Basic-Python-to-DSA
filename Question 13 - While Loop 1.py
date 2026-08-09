596ffdf2dbdc3e30099f69a833108b41e020bea0
Laukik Dudhe <laukikdudhe31@gmail.com>
2026-08-09 18:41:10 +0530

    Add files via upload

---
+++ Question 13 - While Loop 1.py
@@ -0,0 +1,10 @@
+#Problem:
+# Print numbers from 10 to 1 using a while loop.
+# Expected Output:
+# 10,9,8,7,6,5,4,3,2,1
+#Solution:-
+
+i=10
+while(i>=1):
+    print(i)
+    i=i-1
---
+++ Question 14 - While Loop with User Input.py
@@ -0,0 +1,18 @@
+# Problem:
+# Take a number from the user.
+# Print numbers from 1 up to that number.
+# Example:
+# Input:
+# 5
+# Output:
+# 1
+# 2
+# 3
+# 4
+# 5
+
+num=int(input("Enter your number:-"))
+i=1
+while num>=i:
+    print(i)
+    i=i+1
