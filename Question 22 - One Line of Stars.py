3db46558a9f0d7e6f46015a07adb5c123d9a0d30
Laukik Dudhe <laukikdudhe31@gmail.com>
2026-08-09 18:43:17 +0530

    Add files via upload

---
+++ Question 22 - One Line of Stars.py
@@ -0,0 +1,12 @@
+# Problem:
+# Print the following pattern:
+# *****
+# Rules:
+# 1. Use a while loop.
+# 2. Do NOT use a for loop.
+# 3. Print all stars on the same line.
+
+i=1
+while(i<=5):
+    print(end="*")
+    i=i+1
