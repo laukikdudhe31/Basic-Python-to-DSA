6c36fdbb2ec46acc301b297e96f4d6b75ffdfb89
Laukik Dudhe <laukikdudhe31@gmail.com>
2026-08-09 18:43:31 +0530

    Add files via upload

---
+++ Question 23 - Square Pattern (Nested Loop).py
@@ -0,0 +1,22 @@
+# Problem:
+# Print the following pattern:
+# *****
+# *****
+# *****
+# *****
+# *****
+# Rules:
+# 1. Use only while loops.
+# 2. Do NOT use a for loop.
+# 3. Use two while loops (Nested Loop).
+
+i=1
+while(i<=5):
+    j=1
+    while(j<=5):
+        print("*" , end="")
+        j=j+1
+    
+        
+    print()
+    i=i+1
